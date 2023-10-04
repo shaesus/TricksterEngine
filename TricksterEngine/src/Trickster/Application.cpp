0ad2b3a55172f303c492fd23b3e9ba82fde2a5ab
shaesus <115086171+shaesus@users.noreply.github.com>
2023-10-04 22:47:00 +0300

    Window Events

--- TricksterEngine/src/Trickster/Application.cpp
+++ TricksterEngine/src/Trickster/Application.cpp
@@ -1,16 +1,18 @@
 #include "tspch.h"
 #include "Application.h"
 
-#include "Trickster/Events/ApplicationEvent.h"
 #include "Trickster/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Trickster {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
@@ -18,12 +20,28 @@ namespace Trickster {
 
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		TS_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{
 		while (m_Running)
 		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 
 }
